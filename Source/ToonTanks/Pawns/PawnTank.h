6526ff9b5e0e3593aad61779153ce2e707e88292
Rodrigo Farinango <47066093+rodrigofarii@users.noreply.github.com>
2021-03-15 17:24:29 -0500

    Add Pawn movement input

--- Source/ToonTanks/Pawns/PawnTank.h
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -22,6 +22,18 @@ private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* Camera;
 
+	FVector MoveDirection;
+	FQuat RotationDirection;
+
+	float MoveSpeed = 100.0f;
+	float RotateSpeed = 100.0f;
+
+	void CalculateMoveInput(float Value);
+	void CalculateRotateInput(float Value);
+
+	void Move();
+	void Rotate();
+
 
 public:
 	APawnTank();
