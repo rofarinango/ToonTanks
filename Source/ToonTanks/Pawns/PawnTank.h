e94ed51db7829a8403fb8b30c4c51bdb800bad02
Rodrigo Farinango <47066093+rodrigofarii@users.noreply.github.com>
2021-03-15 19:10:01 -0500

    Update PawnTank camera

--- Source/ToonTanks/Pawns/PawnTank.h
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -25,7 +25,9 @@ private:
 	FVector MoveDirection;
 	FQuat RotationDirection;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement", meta=(AllowPrivateAccess="true"))
 	float MoveSpeed = 100.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement", meta=(AllowPrivateAccess="true"))	
 	float RotateSpeed = 100.0f;
 
 	void CalculateMoveInput(float Value);
